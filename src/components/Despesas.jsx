321ad6291116a42b266bdf7e2c1167be36da7076
Guilherme-op7 <netoguilherme989@gmail.com>
2025-10-28 17:56:18 -0300

    ok

--- src/components/Despesas.jsx
+++ src/components/Despesas.jsx
@@ -9,30 +9,29 @@ export default function Despesas() {
                 Controle de Caixa &gt; Despesas
             </p>
 
-            <div className="px-15 py-9 flex flex-col w-2xl">
-                <h2 className="text-2xl font-semibold mb-2">Referência</h2>
+            <div className="px-15 py-9 flex flex-col">
+                <h2 className="text-2xl font-semibold mb-5 text-gray-500">Referência</h2>
 
-                <div className="border bg-white border-gray-200 rounded-md px-3 py-2 w-52">
-                    <select>
+                <div className="flex gap-14 justify-center items-center border bg-white border-gray-200 rounded-xl px-3 py-3 w-85">
+                    <p className="text-gray-500 font-semibold">Mês/Ano</p>
+                    <select className="text-gray-500 font-medium">
                         <option>Outubro/2025</option>
                     </select>
                 </div>
 
-                <h2 className="text-2xl font-semibold mt-8 mb-2">
+                <h2 className="text-2xl text-gray-500 font-semibold mt-8 mb-5">
                     Controle de Despesas
                 </h2>
 
                 <div className="bg-white p-6 rounded-xl border border-gray-200 w-4xl flex flex-col">
-
                     <form>
-
                         <div className="flex items-center gap-x-4 mb-4 border-b border-gray-200">
                             <label className="text-gray-700 font-medium w-32 text-sm ">
                                 Conta <span className="text-red-500">*</span>
                             </label>
                             <select
                                 placeholder="Selecione a conta"
-                                className="w-full  border-gray-300 text-sm py-1"
+                                className="w-full border-gray-300 text-sm py-1"
                             />
                         </div>
 
@@ -42,7 +41,7 @@ export default function Despesas() {
                             </label>
                             <select
                                 placeholder="Selecione o tipo da despesa"
-                                className="w-full  border-gray-300 text-sm py-1"
+                                className="w-full border-gray-300 text-sm py-1"
                             />
                         </div>
 
@@ -52,7 +51,7 @@ export default function Despesas() {
                             </label>
                             <input
                                 placeholder="Insira o valor da despesa"
-                                className="w-full  border-gray-300 text-sm py-1"
+                                className="w-full border-gray-300 text-sm py-1"
                             />
                         </div>
 
@@ -62,7 +61,7 @@ export default function Despesas() {
                             </label>
                             <input
                                 placeholder="Insira a quantidade de parcelas"
-                                className="w-full  border-gray-300 text-sm py-1"
+                                className="w-full border-gray-300 text-sm py-1"
                             />
                         </div>
 
@@ -70,44 +69,70 @@ export default function Despesas() {
                             <label className="text-gray-700 font-medium w-32 text-sm">
                                 Data <span className="text-red-500">*</span>
                             </label>
-                            <input
-                                type="date"
-                                className="w-full  border-gray-300 text-sm py-1"
-                            />
+                            <input type="date" className="w-full border-gray-300 text-sm py-1" />
                         </div>
 
-                        <div className=" gap-x-8 border-b border-gray-200 flex items-center">
+                        <div className="gap-x-8 border-b border-gray-200 flex items-center">
                             <label className="font-medium text-gray-700">Observações</label>
                             <textarea
-                                className=" w-full text-sm pt-5 m-1"
+                                className="w-full text-sm pt-5 m-1"
                                 placeholder="Informe detalhes da despesa"
                             />
                         </div>
 
-                        <div className=" border-b border-gray-200 flex items-center gap-x-4 mb-4 mt-4">
+                        <div className="border-b border-gray-200 flex items-center gap-x-4 mb-4 mt-4">
                             <label className="text-gray-700 font-medium w-32 text-sm">
                                 Recibo <span className="text-red-500">*</span>
                             </label>
                             <input
-                                placeholder="Selecione o Arquivo recebido"
+                                placeholder="Selecione o arquivo recebido"
                                 type="file"
-                                className="w-full  border-gray-300 text-sm py-1"
+                                className="w-full border-gray-300 text-sm py-1"
                             />
                         </div>
 
                         <div className="flex justify-end mt-8">
-                            <button className="w-40 bg-blue-400 text-white rounded-sm py-2 px-4 flex items-center justify-center">
+                            <button className="w-40 bg-blue-400 text-white rounded-md py-2 px-4 flex items-center justify-center hover:bg-blue-500 transition">
                                 Registrar
                             </button>
                         </div>
                     </form>
                 </div>
-
-                <h2 className="text-2xl font-semibold mt-8">
+                <h2 className="text-2xl text-gray-500 font-semibold mt-8">
                     Despesas de Outubro/2025
                 </h2>
-                <div className="mt-2 border border-gray-200 rounded-md p-4 text-gray-400 text-sm">
-                    Nenhum registro encontrado
+
+                <div className="mt-3 rounded-xl border border-gray-200 bg-white w-4xl">
+                    <table className="min-w-full flex flex-col">
+                        <thead className=" bg-gray-100">
+                            <tr className="text-sm font-semibold text-gray-300 ">
+                                <th className="px-6 py-3">
+                                    <input type="checkbox" className="accent-blue-500" />
+                                </th>
+                                <th className="">
+                                    Conta <span className="text-xs">⇅</span>
+                                </th>
+                                <th className="">
+                                    Tipo <span className="text-xs">⇅</span>
+                                </th>
+                                <th className="">
+                                    Valor <span className="text-xs">⇅</span>
+                                </th>
+                                <th className="">
+                                    Data <span className="text-xs">⇅</span>
+                                </th>
+                                <th className="text-center w-6">⋮</th>
+                            </tr>
+                        </thead>
+
+                        <tbody className="text-gray-400 text-md divide-y divide-gray-200">
+                            <tr>
+                                <td className="px-6 py-6 text-center">
+                                    Nenhum registro encontrado
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
